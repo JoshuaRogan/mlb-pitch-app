eca01c90b87b519e9615eb168b92ef39936d8e23
Josh <jrogan@wikia-inc.com>
2018-10-28 19:37:00 -0700

    Placeholder

--- src/App.js
+++ src/App.js
@@ -9,17 +9,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            MLB Pitch App Coming Soon!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <div style={{fontSize: 10}}>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
         </header>
+
       </div>
     );
   }
