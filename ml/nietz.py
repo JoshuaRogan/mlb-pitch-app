67100ba0e31ec740ddb98f52e2b4f3e7db9ce773
Josh <jrogan@wikia-inc.com>
2018-10-30 02:13:10 -0700

    WIP

--- ml/nietz.py
+++ ml/nietz.py
@@ -16,7 +16,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.optimizers import RMSprop
-from keras.utils.data_utils import get_file
+from kerasaa.utils.data_utils import get_file
 import numpy as np
 import random
 import sys
